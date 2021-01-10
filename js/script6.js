a54a024c1ce26dc83b0906bc3109dac772ade612
Elena1717 <elena.parkhomenko17@gmail.com>
2021-01-10 18:01:59 +0200

    first commit

---
+++ js/script6.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//6
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+
+const persone = "5";
+
+const bool = true;
+
+
+let und;
+console.log(und);
+
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+
+// массив
+let arr = ['blum.pmg', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
